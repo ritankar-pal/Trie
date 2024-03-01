2c13b7bb3ff410d4c425de130fce68f5371ff1e1
ritankar pal <ritankar0425@gmail.com>
2024-03-01 02:12:49 -0500

    Remove Word

--- src/in/dsa/cn/Trie.java
+++ src/in/dsa/cn/Trie.java
@@ -23,4 +23,58 @@ public class Trie {
 		this.root = new TrieNode('\0');
 	}
 	
+	private void addHelper(String word, TrieNode root) {
+		
+		if(word.length() == 0) {
+			root.isTerminal = true;
+			return;
+		}
+		
+		int childIndex = word.charAt(0) - 'a';
+		
+		TrieNode child = root.children[childIndex];
+		if(child == null) {
+			child = new TrieNode(word.charAt(0));
+			root.children[childIndex] = child;
+		}
+		
+		addHelper(word.substring(1), child);
+		
+	}
+	
+	public void add(String word) {
+		addHelper(word, root);
+	}
+	
+	public void search(String word) {
+		
+	}
+	
+	
+	private void removeWordHelper(TrieNode root, String word) {
+		
+		//base case:
+		if(word.length() == 0) {
+			root.isTerminal = false;
+			return;
+		}
+	
+		int childIndex = word.charAt(0) - 'a';
+		
+		TrieNode child = root.children[childIndex];
+		if(child == null) return;  //the word doesn't exist hence nothing to remove:
+		
+		removeWordHelper(child, word);
+		
+	}
+	
+	public void removeWord(String word) {
+		removeWordHelper(root, word);
+	}
+
+	
 }
+
+
+
+
